ea3afde0eb1610e3d1434dc0f780d0014499e4f6
Ana Laura Silva <ana.laura.scAL@gmail.com>
2021-12-08 16:55:55 -0300

    first commit and configs

---
+++ src/main/java/br/com/adminediaristas/AdminEdiaristasApplication.java
@@ -0,0 +1,13 @@
+package br.com.adminediaristas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminEdiaristasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminEdiaristasApplication.class, args);
+    }
+
+}
