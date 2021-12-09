79005e64991a477979558a37fff2188ee1cce118
Ana Laura Silva <ana.laura.scAL@gmail.com>
2021-12-09 14:12:23 -0300

    criar action de cadastro de serviço

--- src/main/java/br/com/adminediaristas/web/controllers/ServicoController.java
+++ src/main/java/br/com/adminediaristas/web/controllers/ServicoController.java
@@ -2,15 +2,26 @@ package br.com.adminediaristas.web.controllers;
 
 import br.com.adminediaristas.core.enums.IconeEnum;
 import br.com.adminediaristas.core.models.Servico;
+import br.com.adminediaristas.core.repositories.ServicoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/admin/servicos")
+@RequiredArgsConstructor
 public class ServicoController {
+
+    private final ServicoRepository repository;
+
+    @ModelAttribute("icones")
+    public IconeEnum[] getIcones() {
+        return IconeEnum.values();
+    }
 
     @GetMapping("/cadastrar")
     public ModelAndView cadastrar() {
@@ -20,8 +31,10 @@ public class ServicoController {
         return modelAndView;
     }
 
-    @ModelAttribute("icones")
-    public IconeEnum[] getIcones() {
-        return IconeEnum.values();
+    @PostMapping("/cadastrar")
+    public String cadastrar(Servico servico) {
+        repository.save(servico);
+
+        return "redirect:/admin/servicos/cadastrar";
     }
 }
