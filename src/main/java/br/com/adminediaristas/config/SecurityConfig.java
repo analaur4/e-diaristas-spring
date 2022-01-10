a6aeadeb9452d071a4703e2264570e05ff5447d9
Ana Laura Silva <ana.laura.scAL@gmail.com>
2022-01-10 14:12:03 -0300

    Configurar função de lembrar-me

--- src/main/java/br/com/adminediaristas/config/SecurityConfig.java
+++ src/main/java/br/com/adminediaristas/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.adminediaristas.config;
 
 import br.com.adminediaristas.core.enums.TipoUsuarioEnum;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -20,6 +21,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final PasswordEncoder passwordEncoder;
 
+    @Value("${br.com.treinaweb.ediaristas.rememberme.key}")
+    private String rememberMeKey;
+
+    @Value("${br.com.treinaweb.ediaristas.rememberme.validitySeconds}")
+    private int rememberMeValiditySeconds;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService)
@@ -33,13 +40,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
         http.formLogin()
-            .loginPage("/admin/login")
-            .usernameParameter("email")
-            .passwordParameter("senha")
+                .loginPage("/admin/login")
+                .usernameParameter("email")
+                .passwordParameter("senha")
                 .defaultSuccessUrl("/admin/servicos")
-            .permitAll();
+                .permitAll();
 
         http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout", "GET"));
+
+        http.rememberMe()
+                .tokenValiditySeconds(rememberMeValiditySeconds)
+                .key(rememberMeKey)
+                .rememberMeParameter("lembrar-me");
     }
 
     @Override
